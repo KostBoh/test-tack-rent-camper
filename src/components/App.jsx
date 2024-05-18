29520584fc30df7ae4a78c712ad17de7c541fcb9
KostBoh <bohdan.kostya@gmail.com>
2024-05-18 17:12:29 +0300

    add fix routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import CatalogPage from './pages/CatalogPage/CatalogPage';
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import HomePage from '../pages/HomePage/HomePage';
+import CatalogPage from '../pages/CatalogPage/CatalogPage';
+import FavoritesPage from '../pages/FavoritesPage/FavoritesPage';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/catalog" element={<CatalogPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <a href="/">
+            <li>Home</li>
+          </a>
+          <a href="/catalog">
+            <li>Catalog</li>
+          </a>
+          <a href="/favorites">
+            <li>Favorites</li>
+          </a>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </>
   );
 };
 
